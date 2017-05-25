1cb698c46426759a9216ec69ad0eac7f8ccfb77f
surajjha <suraj.jha@bookmyshow.com>
2017-05-25 13:10:19 +0530

    added package to project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = process.env.port || 3500;
+var port = process.env.port || 80;
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", function(req, res) {
@@ -21,7 +21,7 @@ app.post("/hello", function (req, res) {
     }
 });
 
-app.listen(port, function (err, res) {
+app.listen(port, function () {
     console.log("listening on port : " + port);
 });
 
@@ -30,7 +30,7 @@ process.on('exit', function() {
 });
 
 process.on('uncaughtException', function(error) {
-    console.log("exception");
+    console.log("exception", error);
     process.exit(1);
 });
 
